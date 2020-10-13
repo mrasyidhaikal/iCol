6fe1a7953eeb693ebbdce769a0077adac09b465f
Windy <windywu812@gmail.com>
2020-10-13 10:48:33 +0700

    adding cell, adding extension constraint

--- iCol/ViewControllers/ViewController1.swift
+++ iCol/ViewControllers/ViewController1.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController1: UIViewController {
+class ViewController1: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupNavigation()
+        setupTableView()
     }
 
+    private func setupNavigation() {
+        navigationItem.title = "Habit"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupTableView() {
+        tableView.register(HabitCell.self, forCellReuseIdentifier: HabitCell.reuseIdentifier)
+        tableView.rowHeight = 100
+        tableView.separatorStyle = .none
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HabitCell.reuseIdentifier, for: indexPath) as! HabitCell
+        cell.titleLabel.text = "Test"
+        return cell
+    }
 
 }
 
