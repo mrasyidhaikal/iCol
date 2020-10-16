937d78df0d32d9a1a8f0edffd412d1fd709bbbe6
Windy <windywu812@gmail.com>
2020-10-16 14:39:28 +0700

    finish detail habit

--- iCol/ViewControllers/HabitDetailViewController.swift
+++ iCol/ViewControllers/HabitDetailViewController.swift
@@ -43,6 +43,9 @@ class HabitDetailViewController: UIViewController {
 class ChallengeProgressView: UIView {
     
     let dateLabel = UILabel()
+    var totalEatPerDay: Int = 0
+    let totalEatPerDayLabel = UILabel()
+    
     let collectionView: UICollectionView = {
         let flow = UICollectionViewFlowLayout()
         flow.scrollDirection = .horizontal
@@ -62,25 +65,66 @@ class ChallengeProgressView: UIView {
         dateLabel.font = .preferredFont(forTextStyle: .headline)
         addSubview(dateLabel)
         
+        addSubview(collectionView)
+        
+        let howMuchLabel = UILabel()
+        howMuchLabel.text = "How much do you eat?"
+        addSubview(howMuchLabel)
+        
+        totalEatPerDayLabel.text = "\(totalEatPerDay)"
+        totalEatPerDayLabel.font = .preferredFont(forTextStyle: .title1)
+        totalEatPerDayLabel.font = .boldSystemFont(ofSize: 34)
+        
+        let minButton = UIButton(type: .system)
+        minButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        minButton.addTarget(self, action: #selector(handleMinus), for: .touchUpInside)
+        
+        let plusButton = UIButton(type: .system)
+        plusButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        plusButton.addTarget(self, action: #selector(handlePlus), for: .touchUpInside)
+        
+        let stackView = UIStackView(arrangedSubviews: [minButton, totalEatPerDayLabel, plusButton])
+        stackView.axis = .horizontal
+        stackView.spacing = 32
+        addSubview(stackView)
+        
         titleLabel.setConstraint(topAnchor: topAnchor, topAnchorConstant: 0,
                                  leadingAnchor: leadingAnchor, leadingAnchorConstant: 0,
                                  trailingAnchor: trailingAnchor, trailingAnchorConstant: 0)
         dateLabel.setConstraint(topAnchor: titleLabel.bottomAnchor, topAnchorConstant: 16,
                                 leadingAnchor: leadingAnchor, leadingAnchorConstant: 0,
                                 trailingAnchor: trailingAnchor, trailingAnchorConstant: 0)
-        
         setupCollectionView()
+
+        howMuchLabel.setConstraint(topAnchor: collectionView.bottomAnchor, topAnchorConstant: 32,
+                                   leadingAnchor: leadingAnchor, leadingAnchorConstant: 0)
+
+        stackView.setConstraint(topAnchor: howMuchLabel.bottomAnchor, topAnchorConstant: 32,
+                                bottomAnchor: bottomAnchor, bottomAnchorConstant: 0,
+                                leadingAnchor: leadingAnchor, leadingAnchorConstant: 0)
+
+    }
+    
+    @objc private func handleMinus() {
+        totalEatPerDay -= 1
+        if totalEatPerDay <= 0 {
+            totalEatPerDay = 0
+        }
+        totalEatPerDayLabel.text = "\(totalEatPerDay)"
+    }
+    
+    @objc private func handlePlus() {
+        totalEatPerDay += 1
+        totalEatPerDayLabel.text = "\(totalEatPerDay)"
     }
     
     private func setupCollectionView() {
-        addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.setConstraint(topAnchor: dateLabel.bottomAnchor, topAnchorConstant: 16,
-                                     bottomAnchor: bottomAnchor,
                                      leadingAnchor: leadingAnchor, leadingAnchorConstant: 0,
                                      trailingAnchor: trailingAnchor, trailingAnchorConstant: 0)
-        collectionView.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: 112).isActive = true
         
         collectionView.register(DayCell.self, forCellWithReuseIdentifier: DayCell.reuseIdentifier)
         collectionView.delegate = self
@@ -117,12 +161,16 @@ class DayCell: UICollectionViewCell {
     
     let dateLabel = UILabel()
     let monthLabel = UILabel()
+    let tickButton = UIButton(type: .system)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = #colorLiteral(red: 0.8850145936, green: 0.9960160851, blue: 0.9677742124, alpha: 1)
-        layer.cornerRadius = 8
+        let containerView = UIView()
+        containerView.backgroundColor = #colorLiteral(red: 0.8850145936, green: 0.9960160851, blue: 0.9677742124, alpha: 1)
+        containerView.layer.cornerRadius = 8
+
+        addSubview(containerView)
         
         dateLabel.font = .preferredFont(forTextStyle: .body)
         dateLabel.font = .boldSystemFont(ofSize: 17)
@@ -130,14 +178,25 @@ class DayCell: UICollectionViewCell {
         monthLabel.font = .preferredFont(forTextStyle: .footnote)
         monthLabel.text = "Oct"
         
+        tickButton.setImage(UIImage(systemName: "person.circle"), for: .normal)
+        containerView.addSubview(tickButton)
+        
         let stackView = UIStackView(arrangedSubviews: [dateLabel, monthLabel])
         stackView.axis = .vertical
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.alignment = .center
-        addSubview(stackView)
+        containerView.addSubview(stackView)
         
-        stackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        stackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        containerView.setConstraint(topAnchor: topAnchor, topAnchorConstant: 0,
+                                    leadingAnchor: leadingAnchor, leadingAnchorConstant: 0,
+                                    trailingAnchor: trailingAnchor, trailingAnchorConstant: 0)
+        containerView.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        
+        stackView.setConstraint(centerXAnchor: containerView.centerXAnchor, centerXAnchorConstant: 0,
+                                centerYAnchor: containerView.centerYAnchor, centerYAnchorConstant: 0)
+        
+        tickButton.setConstraint(topAnchor: stackView.bottomAnchor, topAnchorConstant: 32,
+                                 bottomAnchor: bottomAnchor, bottomAnchorConstant: 0,
+                                 centerXAnchor: containerView.centerXAnchor, centerXAnchorConstant: 0)
     }
     
     required init?(coder: NSCoder) {
@@ -151,7 +210,7 @@ class ChallengeOverview: UIView {
     let percentageLabel = UILabel()
     let remainingDay = UILabel()
     let progressBar = ProgressBarView()
-    let totalEat = UILabel()
+    var totalEat = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
